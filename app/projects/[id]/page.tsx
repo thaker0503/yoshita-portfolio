7df1cf63a9698c8cc53aa99b9a550b8971d9ccd0
thaker0503 <thaker.yatharth.05@gmail.com>
2024-06-14 01:06:09 +0530

    Refactor project page layout and add close button

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Carousel } from "flowbite-react";
 import Navbar from "@/components/Navbar";
+import { MdOutlineClose } from "react-icons/md";
 
 
 export default function Page({ params }: { params: { id: string } }) {
@@ -15,38 +16,47 @@ export default function Page({ params }: { params: { id: string } }) {
     if (!data) return <div>Loading...</div>
     return <>
         <Navbar />
-        {data.map((project) => (
-            <div key={project._id} className="mx-2 py-5 md:mx-20 md:py-10 flex flex-col gap-10">
-                {/* create a carousel using tailwindcss */}
-                <div className="h-56 sm:h-[25rem] xl:h-[35rem] 2xl:h-[50rem]">
-                    <Carousel>
-                        {project.carouselImages.map((image: any) => (
-                            <img key={image} src={image} alt={project.title} width={800} height={703} />
-                        ))}
-                    </Carousel>
-                    {/* {project.carouselImages.map((image: any) => (
+        <section
+            className="pt-20"
+        >
+            <div className="flex justify-end mx-2 md:mx-20 mt-2">
+                <a href="/#projects" className="text-4xl text-primary">
+                    <MdOutlineClose />
+                </a>
+            </div>
+            {data.map((project) => (
+                <div key={project._id} className="mx-2 py-5 md:mx-20 md:py-10 flex flex-col gap-10">
+                    {/* create a carousel using tailwindcss */}
+                    <div className="h-56 sm:h-[25rem] xl:h-[35rem] 2xl:h-[50rem]">
+                        <Carousel>
+                            {project.carouselImages.map((image: any) => (
+                                <img key={image} src={image} alt={project.title} width={800} height={703} />
+                            ))}
+                        </Carousel>
+                        {/* {project.carouselImages.map((image: any) => (
                         <Image key={image} src={image} alt={project.title} width={800} height={703} />
                     ))} */}
+                    </div>
+                    <h1
+                        className="text-4xl font-bold text-secondary"
+                    >{project.title}</h1>
+                    <div className="flex flex-col gap-5">
+                        {project.description.map((desc: any) => (
+                            <p key={desc}
+                                className="text-xl text-primary"
+                            >{desc}</p>
+                        ))}
+                    </div>
+                    <h2
+                        className="text-2xl font-bold text-secondary"
+                    >Software Used</h2>
+                    <p className="flex flex-wrap gap-5 flex-col md:flex-row">
+                        {project.software.map((software: any) => (
+                            <span key={software} className="text-xl text-primary">{software}</span>
+                        ))}
+                    </p>
                 </div>
-                <h1
-                    className="text-4xl font-bold text-secondary"
-                >{project.title}</h1>
-                <div className="flex flex-col gap-5">
-                    {project.description.map((desc: any) => (
-                        <p key={desc} 
-                            className="text-xl text-primary"
-                        >{desc}</p>
-                    ))}
-                </div>
-                <h2
-                    className="text-2xl font-bold text-secondary"
-                >Software Used</h2>
-                <p className="flex flex-wrap gap-5 flex-col md:flex-row">
-                    {project.software.map((software: any) => (
-                        <span key={software} className="text-xl text-primary">{software}</span>
-                    ))}
-                </p>
-            </div>
-        ))}
+            ))}
+        </section>
     </>
 }
